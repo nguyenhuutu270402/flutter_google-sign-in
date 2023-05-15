4d75870ebb0938b600b0f4d9aaa41d4ac3323c54
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-05-15 12:04:19 +0700

    update 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +15,90 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'WhatsApp Me',
       home: Scaffold(
-        body: Center(
-          child: Text('Demo'),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((event) {
+      setState(() {
+        _currentUser = event;
+      });
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Future<void> siginInGG() async {
+    try {
+      var value = await _googleSignIn.signIn();
+      print(">>>uuuuu: ${value}");
+    } catch (e) {
+      print("Error sigin GG: $e");
+    }
+  }
+
+  void siginOutGG() {
+    _googleSignIn.disconnect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GoogleSignInAccount? user = _currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Google Sign in"),
+      ),
+      body: Center(
+        child: Container(
+          child: user == null
+              ? Column(
+                  children: [
+                    Text("Chưa đăng nhập"),
+                    ElevatedButton(
+                      onPressed: siginInGG,
+                      child: Text("Sign in"),
+                    ),
+                  ],
+                )
+              : Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: GoogleUserCircleAvatar(identity: user),
+                        title: Text(user.displayName ?? ""),
+                        subtitle: Text(user.email),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("Signed in successfully!"),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: siginOutGG,
+                        child: Text("Sign out"),
+                      ),
+                    ],
+                  ),
+                ),
         ),
       ),
     );
